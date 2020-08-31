b8f1013307ee37140465b8b2f70132fd79302b02
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-09-01 04:12:54 +0700

    Login and Register

---
+++ src/components/Dashboard/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="mb-6">Dashboard nya belum rapih</h1>
+    <sign-out />
+  </div>
+</template>
+
+<script>
+  import SignOut from "@/components/Button/SignOut.vue";
+  export default {
+    name: "Dashboard",
+    components: {
+      SignOut,
+    },
+  };
+</script>
