5f7385ede6f7774137725bc0f259de21973cfe32
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-09-01 08:34:17 +0700

    add modal component

--- src/components/Dashboard/Index.vue
+++ src/components/Dashboard/Index.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <h1 class="mb-6">Dashboard nya belum rapih</h1>
+    <add-modal />
     <sign-out />
   </div>
 </template>
 
 <script>
   import SignOut from "@/components/Button/SignOut.vue";
+  import AddModal from "@/components/Modal/AddModal.vue";
   export default {
     name: "Dashboard",
     components: {
       SignOut,
+      AddModal,
     },
+    data() {
+      return {};
+    },
+    methods: {},
   };
 </script>
